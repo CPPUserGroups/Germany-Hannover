d508f11ae6902552d07ac93e7dc6102fc263d2d4
Oliver Bruns <obruns@gmail.com>
2017-03-07 23:38:59 +0100

    stm32f4: add a "minimal" example in C
    Take the code from [1] but dereference symbolic links and simplify (and
    fix) the blinky example to only toggle PG14 in roughly one second
    intervals.
    
    [1] https://github.com/istarc/stm32/tree/master/examples/Template

---
+++ stm32/demo/src/main.c
@@ -0,0 +1,44 @@
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+
+GPIO_InitTypeDef  GPIO_InitStructure;
+
+/**
+  * @brief  Delay Function.
+  * @param  nCount:specifies the Delay time length.
+  * @retval None
+  */
+void Delay(__IO uint32_t nCount)
+{
+  while(nCount--)
+  {
+  }
+}
+
+int main(void)
+{
+  /* GPIOG Periph clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
+
+  /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOG, &GPIO_InitStructure);
+
+  while (1)
+  {
+    /* PD12 to be toggled */
+    GPIO_SetBits(GPIOG, GPIO_Pin_14);
+
+    /* Insert delay */
+    Delay(0x3FFFFF);
+
+    GPIO_ResetBits(GPIOG, GPIO_Pin_14);
+
+    /* Insert delay */
+    Delay(0xFFFFFF);
+  }
+}
